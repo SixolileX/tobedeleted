0ad9e9a73b0e029a722f51126816de5daae41273
s221474420 <aviwenzena@gmail.com>
2022-09-09 19:14:05 +0200

    Aviwe

---
+++ tobedeleted/Controllers/TeacherController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tobedeleted.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult TDash()
+        {
+            return View();
+        }
+        public IActionResult Assessments()
+        {
+            return View();
+        }
+        public IActionResult Assignment()
+        {
+            return View("Assignment");
+        }
+        public IActionResult Announcements()
+        {
+            return View();
+
+        }
+        public IActionResult Reports()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Quizz()
+        {
+            return View();
+        }
+
+    }
+}
