9a1be565251da795d80f4497962eb531f698716e
vuyomaphosa <s219366713@mandela.ac.za>
2022-09-13 11:01:51 +0200

    vuyo

---
+++ tobedeleted/Models/Assignments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tobedeleted.Models
+{
+    public class Assignments
+    {
+        [Key]
+        public int AssignID { get; set; }
+        public string AssignTitle { get; set; }
+        public string AssignType { get; set; }
+        public string AssignInstructions { get; set; }
+        public string DueDate { get; set; }
+    }
+}
