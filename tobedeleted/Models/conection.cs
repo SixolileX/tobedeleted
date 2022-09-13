be19997cdcc9f88cdc94083bd5dbaa162b5530f4
SixolileX <s220722714@mandela.ac.za>
2022-09-13 15:21:16 +0200

    migration

--- tobedeleted/Models/conection.cs
+++ tobedeleted/Models/conection.cs
@@ -5,13 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace tobedeleted.Models
-{
-    public class conection:DbContext
-    {
-        public conection(DbContextOptions<conection> options) : base(options)
-        {
 
-        }
-        public DbSet<Grade> Grade { get; set; }
-    }
+{
+    //    public class conection:DbContext
+    //    {
+    //        public conection(DbContextOptions<conection> options) : base(options)
+    //        {
+
+    //        }
+    //        public DbSet<Grade> Grade { get; set; }
+    //    }
 }
