59ad4150ec77382e4e0178c587d2d5173c8de200
SixolileX <s220722714@mandela.ac.za>
2022-09-14 07:53:52 +0200

    grades

---
+++ tobedeleted/Migrations/20220913134927_Grades.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace tobedeleted.Migrations
+{
+    public partial class Grades : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
