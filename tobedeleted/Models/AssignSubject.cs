9daee767970b486322e50d9eb3e9de534ae7f82c
SixolileX <s220722714@mandela.ac.za>
2022-10-04 23:27:56 +0200

    assign

---
+++ tobedeleted/Models/AssignSubject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tobedeleted.Models
+{
+    public class AssignSubject
+    {
+        [Key]
+        public int SubID { get; set; }
+   
+        public string SubDesc { get; set; }
+
+        public string SubCode { get; set; }
+
+    }
+}
