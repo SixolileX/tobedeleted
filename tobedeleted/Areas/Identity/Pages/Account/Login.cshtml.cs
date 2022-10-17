268f3d5bbd1db98024f1d806413b7b3b77ba9a88
SixolileX <s220722714@mandela.ac.za>
2022-10-17 02:00:52 +0200

    fixes

--- tobedeleted/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ tobedeleted/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using tobedeleted.Models;
 
 namespace tobedeleted.Areas.Identity.Pages.Account
 {
@@ -19,13 +20,13 @@ namespace tobedeleted.Areas.Identity.Pages.Account
    
     public class LoginModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager,
+        public LoginModel(SignInManager<ApplicationUser> signInManager,
             ILogger<LoginModel> logger,
-            UserManager<IdentityUser> userManager)
+            UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
