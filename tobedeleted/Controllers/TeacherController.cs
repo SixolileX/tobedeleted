cfa766352840b1bbe81ea4426cc23a70f3491cb0
s221474420 <aviwenzena@gmail.com>
2022-10-18 08:04:05 +0200

    gg

--- tobedeleted/Controllers/TeacherController.cs
+++ tobedeleted/Controllers/TeacherController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using tobedeleted.Data;
