21f06b9d10ede25ab4b5e5508fa5137aada4ffb5
vuyomaphosa <s219366713@mandela.ac.za>
2022-10-21 16:24:55 +0200

    m

--- tobedeleted/Models/StuMark.cs
+++ tobedeleted/Models/StuMark.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,11 +11,17 @@ namespace tobedeleted.Models
     {
         [Key]
         public int MarkId { get; set; }
+        [DisplayName("Student Name")]
         public string LearnerIdUser { get; set; }
+        [DisplayName("Subject Name")]
         public string SubjectName { get; set; }
+        [DisplayName("Term 1")]
         public string Term1 { get; set; }
+        [DisplayName("Term 2")]
         public string Term2 { get; set; }
+        [DisplayName("Term 3")]
         public string Term3 { get; set; }
+        [DisplayName("Term 4")]
         public string Term4 { get; set; }
     }
 }
