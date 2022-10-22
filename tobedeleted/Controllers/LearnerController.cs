0c5788f5837890f901ba3380da7727c9a1349032
SixolileX <s220722714@mandela.ac.za>
2022-10-22 08:56:14 +0200

    ss

--- tobedeleted/Controllers/LearnerController.cs
+++ tobedeleted/Controllers/LearnerController.cs
@@ -178,11 +178,12 @@ namespace Inn_TuneProject.Controllers
 
             return View();
         }
-        public ActionResult Resultss()
+        public ActionResult Resultss(string searching)
         {
+            IEnumerable<StuMark> objList = _db.StuMarks;
+            return View(_db.StuMarks.Where(x => x.LearnerIdUser.Contains(searching) || searching == null).ToList());
 
-
-            return View();
+            
         }
         public ActionResult Maths()
         {
