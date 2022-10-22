0b8dfdf8593da81fe6fb3f8c82e372e404605cc3
s221474420 <aviwenzena@gmail.com>
2022-10-22 02:55:05 +0200

    jhjhh

---
+++ tobedeleted/Controllers/MediaTypeController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using tobedeleted.Data;
+using tobedeleted.Entities;
+
+namespace tobedeleted.Controllers
+{
+    public class MediaTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MediaTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MediaType
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MediaType.ToListAsync());
+        }
+
+        // GET: MediaType/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mediaType = await _context.MediaType
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mediaType == null)
+            {
+                return NotFound();
+            }
+
+            return View(mediaType);
+        }
+
+        // GET: MediaType/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MediaType/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ThumbnailmagePath")] MediaType mediaType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mediaType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mediaType);
+        }
+
+        // GET: MediaType/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mediaType = await _context.MediaType.FindAsync(id);
+            if (mediaType == null)
+            {
+                return NotFound();
+            }
+            return View(mediaType);
+        }
+
+        // POST: MediaType/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ThumbnailmagePath")] MediaType mediaType)
+        {
+            if (id != mediaType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mediaType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MediaTypeExists(mediaType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mediaType);
+        }
+
+        // GET: MediaType/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mediaType = await _context.MediaType
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mediaType == null)
+            {
+                return NotFound();
+            }
+
+            return View(mediaType);
+        }
+
+        // POST: MediaType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mediaType = await _context.MediaType.FindAsync(id);
+            _context.MediaType.Remove(mediaType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MediaTypeExists(int id)
+        {
+            return _context.MediaType.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ tobedeleted/Views/Teacher/Model/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tobedeleted.Views.Teacher.Model
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
