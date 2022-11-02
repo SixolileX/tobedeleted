e126e775d1f4d3110a13fdf124fa9477d27af995
Rorisang Itumeleng <s221403183@mandela.ac.za>
2022-11-02 15:53:25 +0200

    Distinct Reporting

--- tobedeleted/Controllers/HODController.cs
+++ tobedeleted/Controllers/HODController.cs
@@ -713,6 +713,7 @@ namespace tobedeleted.Controllers
                               where D.DepID == H.DepID && S.DepID == H.DepID
                                     && SG.SubId == S.SubID && s.SubID == S.SubID && SG.GrID == G.GrID
                                     && H.userHoDId == U.Id && UR.UserId == U.Id && T.DepID==S.DepID && H.DepID==T.DepID 
+                                    && H.userHoDId==users
                                   select new MyHODReport { Department = D, Subject = S, HODs = H, User = U, Grade = G, AssignSubjectGrade = SG, TimeTable=T  }).Distinct().ToList();
             return View();
             }
